ccd778dfd5a0fd9f9b90fa7836abe72eeb08b285
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-28 23:39:58 -0300

    Update dictionary_comprehension.py

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -1,1 +1,22 @@
-lista = []
+lista = [
+    ("chave","valor"),
+    ("chave2","valor2")
+]
+
+d1 = dict(lista)
+print(lista)
+print()
+###terá o mesmo resultado que:
+
+d2 = {x:y for x,y in lista} #porém, aqui podemos fazer mais coisas, como multiplicar....
+print(d2)
+print()
+
+##ex:
+
+d3 = {x : y * 2 for x,y in lista}
+d4 = {x * 2 : y * 2 for x,y in lista}
+d5 = {x * 2 : y for x,y in lista}
+print(d3)
+print(d4)
+print(d5)
