68e0b803eac10609811bea1e35c600e573903fec
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-28 23:52:44 -0300

    Update dictionary_comprehension3.py

--- dictionary_comprehension3.py
+++ dictionary_comprehension3.py
@@ -6,6 +6,6 @@ d2 = {x for x in range(5)}
 print(d2)
 print()
 
-d3 = {f'chave_{x}': x**2 for x in range(5)}
+d3 = {f'chave_{x}': x**2 for x in range(5)} ##adiciona a string 'chave', fa
 print(d3)
 print()
