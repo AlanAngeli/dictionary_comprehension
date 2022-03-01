720aa3c92f644115e9881d3af15a2934652fe351
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-02-28 23:42:32 -0300

    Update dictionary_comprehension.py

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -20,3 +20,19 @@ d5 = {x * 2 : y for x,y in lista}
 print(d3)
 print(d4)
 print(d5)
+print()
+##mais exemplos:
+
+lista2 = [
+    ("chave",2),
+    ("chave2",5)
+]
+
+d6 = {x : y * 2 for x,y in lista2}
+d7 = {x * 2 : y * 2 for x,y in lista2}
+d8 = {x * 2 : y for x,y in lista2}
+print(lista2)
+print(d6)
+print(d7)
+print(d8)
+print()
